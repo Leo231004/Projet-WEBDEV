39a01dad6c431949eedd1d3e1a01c3b78f228dbe
hing-thanh <tanvietnam089@gmail.com>
2024-12-16 16:08:53 +0100

    derniere modif

--- js/searchMap.js
+++ js/searchMap.js
@@ -1,3 +1,19 @@
+// panneau deroulant
+document.addEventListener('DOMContentLoaded', () => {
+    const servicesLink = document.querySelector('nav ul li a[href="services.html"]');
+    const servicesDropdown = document.createElement('div');
+    servicesDropdown.classList.add('services-dropdown');
+    
+    servicesDropdown.innerHTML = `
+        <ul>
+            <li><a href="billetterie.html">Billetterie</a></li>
+            <li><a href="recherche.html">Pour vous reperer</a></li>
+        </ul>
+    `;
+    
+    servicesLink.parentElement.appendChild(servicesDropdown);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const map = L.map("map");
 
